bed1266071eb7cec80ed324e7210024fdc69c657
magneee <biancadefilippi54@gmail.com>
2023-10-11 09:34:43 +0200

    added spring initializer

---
+++ src/test/java/it/euris/javaacademy/teslabattery_dfa/TeslabatteryDfaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.euris.javaacademy.teslabattery_dfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeslabatteryDfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
