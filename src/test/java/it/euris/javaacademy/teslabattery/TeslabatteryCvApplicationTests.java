1437385e910c74a50cad088b7f68ff07a6b4b1ff
cujbaVictor <cujba.victor96@gmail.com>
2023-10-11 09:32:00 +0200

    Initial commit.

---
+++ src/test/java/it/euris/javaacademy/teslabattery/TeslabatteryCvApplicationTests.java
@@ -0,0 +1,13 @@
+package it.euris.javaacademy.teslabattery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeslabatteryCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
