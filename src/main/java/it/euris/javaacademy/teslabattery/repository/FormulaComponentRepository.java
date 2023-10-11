b4caac7039219bc19c37647199d1074e228015e8
cujbaVictor <cujba.victor96@gmail.com>
2023-10-11 14:10:12 +0200

    repository and service implementation for FormulaComponent added.

---
+++ src/main/java/it/euris/javaacademy/teslabattery/repository/FormulaComponentRepository.java
@@ -0,0 +1,7 @@
+package it.euris.javaacademy.teslabattery.repository;
+
+import it.euris.javaacademy.teslabattery.data.entity.FormulaComponent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormulaComponentRepository extends JpaRepository<FormulaComponent, Integer> {
+}
